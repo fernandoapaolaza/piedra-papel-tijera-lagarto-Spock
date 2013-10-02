cf114ac04fb64701a4818839d9965c45316d78eb
fer <fernando.apaolaza@gmail.com>
2013-10-02 20:25:41 -0300

    Arreglos esteticos 2

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 public class Mano {
 	private final Forma forma;
 	private final ArrayList<Integer> venceA = new ArrayList<Integer>();
+	private final int tamanoEnum = 5;
 
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será la que determine su
 	 * condición en el juego.
-	 * 
 	 * @param forma
 	 *            , la Forma que adopta la Mano.
 	 */
@@ -24,22 +24,23 @@ public class Mano {
 
 	/**
 	 * Evaluará el resultado de la partida según las reglas del juego.
-	 * 
 	 * @param otra
 	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		if (this.forma.getValor().equals(otra.forma.getValor()))
+		if (this.forma.getValor().equals(otra.forma.getValor())){
 			return Resultado.EMPATA;
-		if (this.venceA.contains(otra.forma.getValor()))
+		}
+		if (this.venceA.contains(otra.forma.getValor())){
 			return Resultado.GANA;
+		}
 		return Resultado.PIERDE;
 	}
 
 	private Integer getAnterior(int offset) {
 		if (this.forma.getValor() - offset < 0) {
-			return (this.forma.getValor() - offset + 5);
+			return (this.forma.getValor() - offset + tamanoEnum);
 		}
 		return this.forma.getValor() - offset;
 	}
