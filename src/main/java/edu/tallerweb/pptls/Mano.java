9b6245f11f49fe7d6ae5e97d7c40d4161db01b7a
fer <fernando.apaolaza@gmail.com>
2013-10-02 20:18:17 -0300

    Arreglos esteticos

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -8,35 +8,40 @@ import java.util.ArrayList;
 public class Mano {
 	private final Forma forma;
 	private final ArrayList<Integer> venceA = new ArrayList<Integer>();
+
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		this.forma=forma;
+		this.forma = forma;
 		this.venceA.add(getAnterior(1));
 		this.venceA.add(getAnterior(2));
 	}
+
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		if(this.forma.getValor().equals(otra.forma.getValor()))
+		if (this.forma.getValor().equals(otra.forma.getValor()))
 			return Resultado.EMPATA;
-		if(this.venceA.contains(otra.forma.getValor()))
+		if (this.venceA.contains(otra.forma.getValor()))
 			return Resultado.GANA;
 		return Resultado.PIERDE;
 	}
 
-	private Integer getAnterior(int offset){
-		if(this.forma.getValor()-offset<0){
-			return (this.forma.getValor()-offset+5);		
+	private Integer getAnterior(int offset) {
+		if (this.forma.getValor() - offset < 0) {
+			return (this.forma.getValor() - offset + 5);
 		}
-		return this.forma.getValor()-offset;
+		return this.forma.getValor() - offset;
 	}
 
 }
